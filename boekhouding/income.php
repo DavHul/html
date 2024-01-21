bd32f8c5b260ff65adaa2eeca591d2d4264e6959
DavidHul <80717144+DavHul@users.noreply.github.com>
2024-01-21 20:17:59 +0100

    Update income.php

--- boekhouding/income.php
+++ boekhouding/income.php
@@ -5,9 +5,11 @@
         <?php
             include_once './src/login.php';
             $login_class = new login();
+            $login_class->get_db_credentials();
 
             include_once './src/income_class.php';
             $income_class = new income_class();
+            $income_class->get_db_credentials();
         ?>
     </head>
     <body>
@@ -16,6 +18,7 @@
         if ($result == "false"){
             header("Refresh:1; url=index.php");
         }
+
     ?>
     <div class="topnav">
         <a href="./total.php">Home</a>
@@ -29,32 +32,78 @@
     </div>
     <div class="content_upper">
       <h1>Toevoegen/aanpassen</h1>
-      <form name="add_expense" method="post" action="#">
-        Datum: <input type="text" id="date" name="date" required> <br>
-        Titel: <input type="text" id="title" name="title" required> <br>
-        Description: <input type="text" id="description" name="description" required> <br>
-        Bedrag: <input type="number" id="amount" name="amount" required> <br>
-        Oorsprong: <input type="text" id="origin" name="origin" required> <br>
-        Category: <input type="text" id="category" name="category" list="categories" required> <br>
-        <datalist id="categories">
-          <option value="contant">
-          <option value="rekening">
-        </datalist>
-        Afgerond: <input type="radio" id="complete" name="complete" value="complete">Afgerond<input type="radio" id="complete" name="complete" value="incomplete">Nog afronden<br>
-        <input type="submit" value="Toevoegen" name="add_expense">
-      </form>
+
       <?php
-      if (isset($_POST["add_expense"])){
-        $date = htmlspecialchars($_POST["date"]);
-        $title = htmlspecialchars($_POST["title"]);
-        $description = htmlspecialchars($_POST["description"]);
-        $amount = htmlspecialchars($_POST["amount"]);
-        $origin = htmlspecialchars($_POST["origin"]);
-        $complete = htmlspecialchars($_POST["complete"]);
-        $category = htmlspecialchars($_POST["category"]);
-        $income_class->add_income($date, $title, $description, $amount, $origin, $category, $complete);
-        header("Refresh:1; url=income.php");
-      }?>
+      if ($result == "0" OR $result == "1"){
+        if (isset($_POST["change_income"])){
+          $id_number = htmlspecialchars($_POST["id_number"]);
+          echo $id_number;
+          $data = $income_class->get_data($id_number);
+          echo '
+          <form name="add_expense" method="post" action="#">
+          ID: <input type="text" id="id_number" name="id_number" readonly value='.$id_number.'> <br>
+          Datum: <input type="text" id="date" name="date" required value='.$data[1].'> <br>
+          Titel: <input type="text" id="title" name="title" required value='.$data[2].'> <br>
+          Description: <input type="text" id="description" name="description" required value='.$data[3].'> <br>
+          Bedrag: <input type="number" id="amount" name="amount" required value='.$data[4].'> <br>
+          Oorsprong: <input type="text" id="origin" name="origin" required value='.$data[5].'> <br>
+          Category: <input type="text" id="category" name="category" list="categories" required value='.$data[6].'> <br>
+          <datalist id="categories">
+            <option value="contant">
+            <option value="rekening">
+          </datalist>
+          ';
+          if ($data[7] == "incomplete"){
+            echo 'Afgerond: <input type="radio" id="complete" name="complete" value="complete">Afgerond<input type="radio" id="complete" name="complete" value="incomplete" checked>Nog afronden<br>
+              <input type="submit" value="Toevoegen" name="change_income2">
+            </form>';
+          }else{
+            echo 'Afgerond: <input type="radio" id="complete" name="complete" value="complete" checked>Afgerond<input type="radio" id="complete" name="complete" value="incomplete">Nog afronden<br>
+              <input type="submit" value="Toevoegen" name="change_income2">
+            </form>';
+          }
+        }else{
+          echo '
+          <form name="add_expense" method="post" action="#">
+          Datum: <input type="text" id="date" name="date" required> <br>
+          Titel: <input type="text" id="title" name="title" required> <br>
+          Description: <input type="text" id="description" name="description" required> <br>
+          Bedrag: <input type="number" id="amount" name="amount" required> <br>
+          Oorsprong: <input type="text" id="origin" name="origin" required> <br>
+          Category: <input type="text" id="category" name="category" list="categories" required> <br>
+          <datalist id="categories">
+            <option value="contant">
+            <option value="rekening">
+          </datalist>
+          Afgerond: <input type="radio" id="complete" name="complete" value="complete">Afgerond<input type="radio" id="complete" name="complete" value="incomplete">Nog afronden<br>
+          <input type="submit" value="Toevoegen" name="add_expense">
+        </form>';
+        }
+        if (isset($_POST["add_expense"])){
+          $date = htmlspecialchars($_POST["date"]);
+          $title = htmlspecialchars($_POST["title"]);
+          $description = htmlspecialchars($_POST["description"]);
+          $amount = htmlspecialchars($_POST["amount"]);
+          $origin = htmlspecialchars($_POST["origin"]);
+          $complete = htmlspecialchars($_POST["complete"]);
+          $category = htmlspecialchars($_POST["category"]);
+          $income_class->add_income($date, $title, $description, $amount, $origin, $category, $complete);
+          header("Refresh:0; url=income.php");
+        }
+        if (isset($_POST["change_income2"])){
+          $id_number = htmlspecialchars($_POST["id_number"]);
+          $date = htmlspecialchars($_POST["date"]);
+          $title = htmlspecialchars($_POST["title"]);
+          $description = htmlspecialchars($_POST["description"]);
+          $amount = htmlspecialchars($_POST["amount"]);
+          $origin = htmlspecialchars($_POST["origin"]);
+          $complete = htmlspecialchars($_POST["complete"]);
+          $category = htmlspecialchars($_POST["category"]);
+          $income_class->change_income($id_number, $date, $title, $description, $amount, $origin, $category, $complete);
+          header("Refresh:0; url=income.php");
+        }
+      }  
+      ?>
     </div>
     <div class="content_lower">
       <h1>Geschiedenis</h1>
